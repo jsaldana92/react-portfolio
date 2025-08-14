e842f4916781592bd146f7e6d5fb657274019378
Jhonatan Saldana <jsaldana92@gmail.com>
2025-08-14 14:20:29 -0400

    Updates

--- assets/BentoSection-TC4k2bEd.js
+++ assets/BentoSection-TC4k2bEd.js
@@ -1,4 +1,4 @@
-import{a as h0,b as ix,R as R2,r as Ze,j as rt,g as oi}from"./index-D8QZewsj.js";import{f as U2}from"./flutterlogo-BA0L0q-h.js";const D2="/assets/figro_logo-CTtJeDWO.png";/**
+import{a as h0,b as ix,R as R2,r as Ze,j as rt,g as oi}from"./index-G7jeIGcv.js";import{f as U2}from"./flutterlogo-BA0L0q-h.js";const D2="/assets/figro_logo-CTtJeDWO.png";/**
  * @license
  * Copyright 2010-2025 Three.js Authors
  * SPDX-License-Identifier: MIT
